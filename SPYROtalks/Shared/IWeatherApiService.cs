ef98ad75832f7dcf762cb0d8a0256f8f6914a4dc
Lukasz Lawicki <lukasz.lawicki@wp.pl>
2019-01-12 19:19:01 +0100

    Get weather as model not as a string

--- SPYROtalks/Shared/IWeatherApiService.cs
+++ SPYROtalks/Shared/IWeatherApiService.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Refit;
+using SPYROtalks.RefitSample.Models;
 
 namespace SPYROtalks.Shared
 {
     public interface IWeatherApiService
     {
-        //api.openweathermap.org/data/2.5/weather? q = wroclaw & appid = fabffdc626e2fa75b8afba242c296dba
         [Get("/2.5/weather?q={cityName}&appid=fabffdc626e2fa75b8afba242c296dba")]
-        Task<string> GetWeatherForCity(string cityName);
+        Task<WeatherApiResponseModel> GetWeatherForCity(string cityName);
     }
 }
