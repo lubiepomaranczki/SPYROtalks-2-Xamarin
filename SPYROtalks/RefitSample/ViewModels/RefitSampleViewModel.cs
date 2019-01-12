5123286db329297d3facd87846dd02f514a7b255
Lukasz Lawicki <lukasz.lawicki@wp.pl>
2019-01-12 16:49:35 +0100

    Added RefitSample

--- SPYROtalks/RefitSample/ViewModels/RefitSampleViewModel.cs
+++ SPYROtalks/RefitSample/ViewModels/RefitSampleViewModel.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using Refit;
+using SPYROtalks.Shared;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
 namespace SPYROtalks.RefitSample.ViewModels
 {
-    public class RefitSampleViewModel
+    public class RefitSampleViewModel : BaseViewModel
     {
+        private readonly IWeatherApiService weatherApiService;
+
         public RefitSampleViewModel()
         {
+            Title = "Refit sample";
+            weatherApiService = RestService.For<IWeatherApiService>("https://api.openweathermap.org/data");
+        }
 
+        public ICommand GetWeatherCommand => new Command(async (x) => await OnGetWeather(x));
+
+        private async Task OnGetWeather(object obj)
+        {
+            try
+            {
+                var test = await weatherApiService.GetWeatherForCity("wroclaw");
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
